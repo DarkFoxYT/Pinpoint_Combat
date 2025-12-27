699d498cedc607a96a28ca40b90c06ab40252a5e
darkf <shadowmcshort@gmail.com>
2025-12-28 00:28:10 +0100

    init

---
+++ src/client/java/net/goat/client/pinpoint_combatClient.java
@@ -0,0 +1,10 @@
+package net.goat.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class pinpoint_combatClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/net/goat/pinpoint_combat.java
@@ -0,0 +1,10 @@
+package net.goat;
+
+import net.fabricmc.api.ModInitializer;
+
+public class pinpoint_combat implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
